aeb39064f09f2ebf1ef5262bfd32d892f3fe747a
Logan Gunn <lgunn4@uwo.ca>
2023-11-23 09:27:09 -0500

    ✍️ Added blog button

--- src/Components/FrontPage/HeaderSection/HeaderImage.jsx
+++ src/Components/FrontPage/HeaderSection/HeaderImage.jsx
@@ -2,8 +2,19 @@ import {Col, Row} from "react-bootstrap";
 import React from "react";
 import "./HeaderImage.css";
 import {HEADER_IMAGE_URL} from "../../../Assets/Constants";
+import {Button} from "antd";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import { faComment } from "@fortawesome/free-solid-svg-icons";
+
+
 
 function HeaderImage() {
+    const buttonIcon = (
+        <div class="blog-button-icon">
+            <FontAwesomeIcon icon={faComment}/>
+        </div>
+    )
+
     return(
         <div className="header">
             <Row className="header-img-text-row" style={{backgroundImage: `url(${HEADER_IMAGE_URL})`}}>
@@ -11,6 +22,9 @@ function HeaderImage() {
                     <div className="header-img-text-box">
                         <h3>My Name is <strong>Logan Gunn</strong></h3>
                         <h4>I am a full stack developer from London Ontario, Canada </h4>
+                    </div>
+                    <div className="blog-button">
+                        <Button type="primary" size="large" href="https://blog.logangunn.com" icon={buttonIcon}>Visit My Blog</Button>
                     </div>
                 </Col>
             </Row>
