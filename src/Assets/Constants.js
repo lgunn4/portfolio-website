854856d83f8a1e85c30e39e3c1d460d5374fd64e
Logan Gunn <44077842+lgunn4@users.noreply.github.com>
2024-08-09 09:38:01 -0300

    🏃 30% load time reduction

--- src/Assets/Constants.js
+++ src/Assets/Constants.js
@@ -14,7 +14,7 @@ import {
 import {faDatabase, faDharmachakra, faGem, faTrain} from "@fortawesome/free-solid-svg-icons";
 import React from "react";
 
-export const HEADER_IMAGE_URL = "https://res.cloudinary.com/ddtorekqd/image/upload/q_auto/v1700508066/cover_divxjz.jpg?w=auto&h=auto";
+export const HEADER_IMAGE_URL = "https://res.cloudinary.com/ddtorekqd/image/upload/q_auto/f_auto/v1700508066/cover_divxjz.jpg?w=auto&h=auto";
 
 export const javaIcon = (<div className="skill-icon"><FontAwesomeIcon icon={faJava}/></div>);
 export const javaIconText = (<div className="skill-icon"><FontAwesomeIcon icon={faJava}/> Java</div>);
@@ -82,7 +82,7 @@ export const projects = [
             </div>
         ),
         url: "https://apps.shopify.com/translate-and-adapt",
-        image: "https://res.cloudinary.com/ddtorekqd/image/upload/q_auto/v1700504918/maxresdefault_s6vjmz.jpg?w=auto&h=auto",
+        image: "https://res.cloudinary.com/ddtorekqd/image/upload/q_auto/f_auto/v1700504918/maxresdefault_s6vjmz.jpg?w=auto&h=auto",
         skills: [shopifyIcon, rubyIcon, reactIcon, htmlIcon, jsIcon, cssIcon]
     },
     {
@@ -93,7 +93,7 @@ export const projects = [
             </div>
         ),
         url: "https://www.geotab.com/fleet-management-solutions/evsa/",
-        image: "https://res.cloudinary.com/ddtorekqd/image/upload/q_auto/v1700505559/Screen_Shot_2023-11-20_at_1.42.59_PM_hudahv.png?w=auto&h=auto",
+        image: "https://res.cloudinary.com/ddtorekqd/image/upload/q_auto/f_auto/v1700505559/Screen_Shot_2023-11-20_at_1.42.59_PM_hudahv.png?w=auto&h=auto",
         skills: [pythonIcon, mysqlIcon]
     },
     {
@@ -104,7 +104,7 @@ export const projects = [
             </div>
         ),
         url: "https://www.autodatasolutions.com/",
-        image: "https://res.cloudinary.com/ddtorekqd/image/upload/q_auto/q_20/v1610131761/iom-project-image_gkaj1c.jpg?w=auto&h=auto",
+        image: "https://res.cloudinary.com/ddtorekqd/image/upload/q_auto/f_auto/q_20/v1610131761/iom-project-image_gkaj1c.jpg?w=auto&h=auto",
         skills: [javaIcon, pythonIcon, mysqlIcon, dockerIcon]
     },
     {
@@ -113,7 +113,7 @@ export const projects = [
             <div>
                 <p>The Shoppies is a web application designed and built by me that allows users to search for films and add them to a list of nominations.</p>
             </div>),
-        image: "https://res.cloudinary.com/ddtorekqd/image/upload/q_auto/c_scale,q_10,w_1079/v1613152221/ShoppiesSite_hqnxyw.png?w=auto&h=auto",
+        image: "https://res.cloudinary.com/ddtorekqd/image/upload/q_auto/f_auto/c_scale,q_10,w_1079/v1613152221/ShoppiesSite_hqnxyw.png?w=auto&h=auto",
         url: "https://shoppie-movie-awards.netlify.app/",
         skills: [reactIcon,htmlIcon, cssIcon, jsIcon]
     },
@@ -124,7 +124,7 @@ export const projects = [
                 <p>Image repository is a project I designed and built in C# using the .NET framework.</p>
                 <p>It is an API that allows clients to upload, view, and manage images.</p>
             </div>),
-        image: "https://res.cloudinary.com/ddtorekqd/image/upload/q_auto/v1700924656/jon-tyson-P2aOvMMUJnY-unsplash_zatxn2.jpg?w=auto&h=auto",
+        image: "https://res.cloudinary.com/ddtorekqd/image/upload/q_auto/f_auto/v1700924656/jon-tyson-P2aOvMMUJnY-unsplash_zatxn2.jpg?w=auto&h=auto",
         url: "https://github.com/lgunn4/shopify-image-repository",
         skills: [cSharpIcon, microsoftIcon]
     },
@@ -134,7 +134,7 @@ export const projects = [
             <div>
                 <p>The Resourcing App is a web-application that allows managers to keep track of and manage resources on different projects.</p>
             </div>),
-        image: "https://res.cloudinary.com/ddtorekqd/image/upload/q_auto/q_20/v1610131939/resource-management-image_ngok87.jpg?w=auto&h=auto",
+        image: "https://res.cloudinary.com/ddtorekqd/image/upload/q_auto/f_auto/q_20/v1610131939/resource-management-image_ngok87.jpg?w=auto&h=auto",
         url: "https://www.autodatasolutions.com/",
         skills: [javaIcon, mysqlIcon, kubeIcon, reactIcon]
     },
@@ -145,7 +145,7 @@ export const projects = [
                 <p>blog.logangunn.com is a blog website designed and built by me in Ruby on Rails to allow me to blog about what is happening in my life. </p>
             </div>
         ),
-        image: "https://res.cloudinary.com/ddtorekqd/image/upload/q_auto/v1700924416/Screen_Shot_2023-11-25_at_10.03.15_AM_bcxazg.png?w=auto&h=auto",
+        image: "https://res.cloudinary.com/ddtorekqd/image/upload/q_auto/f_auto/v1700924416/Screen_Shot_2023-11-25_at_10.03.15_AM_bcxazg.png?w=auto&h=auto",
         url: "https://blog.logangunn.com",
         skills: [rubyIcon, htmlIcon, jsIcon, cssIcon]
     },
@@ -157,7 +157,7 @@ export const projects = [
                 <p>This website also features projects I have worked on in order to highlight my abilities as a software developer.</p>
             </div>
         ),
-        image: "https://res.cloudinary.com/ddtorekqd/image/upload/q_auto/v1700507209/Screen_Shot_2023-11-20_at_2.10.26_PM_sogptt.png?w=auto&h=auto",
+        image: "https://res.cloudinary.com/ddtorekqd/image/upload/q_auto/f_auto/v1700507209/Screen_Shot_2023-11-20_at_2.10.26_PM_sogptt.png?w=auto&h=auto",
         url: "/",
         skills: [reactIcon,htmlIcon, cssIcon, jsIcon]
     },
@@ -169,7 +169,7 @@ export const projects = [
                 <p>It was a portfolio website to showcase my interests and talents.</p>
             </div>
         ),
-        image: "https://res.cloudinary.com/ddtorekqd/image/upload/q_auto/v1700504046/g9wtrjlvnqug1vywu21z.png?w=auto&h=auto",
+        image: "https://res.cloudinary.com/ddtorekqd/image/upload/q_auto/f_auto/v1700504046/g9wtrjlvnqug1vywu21z.png?w=auto&h=auto",
         url: "https://old.logangunn.com",
         skills: [htmlIcon, jsIcon, cssIcon]
     },
@@ -181,7 +181,7 @@ export const projects = [
                 <p>It was a portfolio website to showcase my interests and talents.</p>
             </div>
         ),
-        image: "https://res.cloudinary.com/ddtorekqd/image/upload/q_auto/v1700506959/Screen_Shot_2023-11-20_at_2.05.25_PM_dfuq0x.png?w=auto&h=auto",
+        image: "https://res.cloudinary.com/ddtorekqd/image/upload/q_auto/f_auto/v1700506959/Screen_Shot_2023-11-20_at_2.05.25_PM_dfuq0x.png?w=auto&h=auto",
         url: "https://dahee.logangunn.com/",
         skills: [htmlIcon, jsIcon, cssIcon]
     },
