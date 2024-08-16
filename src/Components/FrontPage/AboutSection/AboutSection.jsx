f395eaacbe9de2cab04f14eb8b961cce26fc9c93
Logan Gunn <44077842+lgunn4@users.noreply.github.com>
2024-08-16 10:54:30 -0600

    💨 More Fast Images

--- src/Components/FrontPage/AboutSection/AboutSection.jsx
+++ src/Components/FrontPage/AboutSection/AboutSection.jsx
@@ -33,7 +33,7 @@ function AboutSection(){
                     </h5>
                 </Col>
                 <Col className="about-image" md={{span: 2, offset: 1}}>
-                    <Image roundedCircle width={250} height={250} src="https://res.cloudinary.com/ddtorekqd/image/upload/q_auto/f_auto/v1700508074/thumbnail_ryrqu4.jpg" />
+                    <Image roundedCircle width={250} height={250} src="https://res.cloudinary.com/ddtorekqd/image/upload/q_auto:eco/f_auto/w_500/v1700508074/thumbnail_ryrqu4.jpg" />
                 </Col>
 
             </Row>
